09581d1d648afae72f2b4333ca5096860432bd52
Sebastian Scott <sebastian.r.scott@gmail.com>
2019-04-09 03:11:44 +0000

    master: More help, showing up

--- main.c
+++ main.c
@@ -6,17 +6,18 @@ int main()
     char array[] = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
 
 int i = 0; 
-int key = 5;
+int key = 1;
 char No1 = array[0];
 
-printf("%s\n", array);
+//printf("%s\n", array);
 while(i<25)
 {
     //printf("%c, %d, --> %c, %d\n", array[i] , array[i], array[i]+key , array[i]+key);
-    //printf("%s%c\n", array+key, No1);
+    printf("%s%c\n", array, No1);
     i++;
     //key++;
     array[i] = array[i] + key;
+    No1 = array[0];
 }
 
 printf("%s\n", array);
