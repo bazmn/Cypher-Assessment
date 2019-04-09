4fc31498b716d98c6c14465bd2215c9c7c61e9d5
Sebastian Scott <sebastian.r.scott@gmail.com>
2019-04-09 02:45:26 +0000

    master: Playing around with that code

--- main.c
+++ main.c
@@ -6,12 +6,14 @@ int main()
     char array[] = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
 
 int i = 0; 
-int key = 20;
+int key = 1;
 
 while(i<25)
 {
-    printf("%c, %d, --> %c, %d\n", array[i] , array[i], array[i]+key , array[i]+key);
-    i++; 
+    //printf("%c, %d, --> %c, %d\n", array[i] , array[i], array[i]+key , array[i]+key);
+    printf("%s, %d leads to %s\n", array, array[i], array+key);
+    i++;
+    key++;
 }
 }
     
