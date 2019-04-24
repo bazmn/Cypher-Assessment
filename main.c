8c9134c6d4f4a5232c06a8bd02a601578df1b864
Sebastian Scott <sebastian.r.scott@gmail.com>
2019-04-24 04:31:09 +0000

    master: Complete switch case for Cesar

--- main.c
+++ main.c
@@ -1,29 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int i; 
-int key = 10; // how much the array will move
+int key = 14; // how much the array will move
+char A2Z[] = "KWZZ HVWG KCFY DZSOGS?!"; //first array
+
 
 int main()
 {
-    char A2Z[] = "WILL THIS WORK"; //first array
-for (i = 0; A2Z[i] != '\0'; i++) // starting a counter for how ever large A2Z is
-{
-    printf("%c, %d, --> %c, %d\n", A2Z[i] , A2Z[i], A2Z[i]+key , A2Z[i]+key); // displays what is happeing in the workings of the code
+int dkey = 26 - key;    
+switch(2) {
+    case 1:{
+    //printf("Enter code: ");
+    //scanf("%s", &A2Z);
+        for (i = 0; A2Z[i] != '\0'; i++) // starting a counter for how ever large A2Z is
+        {
+        //printf("%c, %d, --> %c, %d\n", A2Z[i] , A2Z[i], A2Z[i]+key , A2Z[i]+key); // displays what is happeing in the workings of the code
+        //printf("%s\n", A2Z);
+            if(A2Z[i] >= 97 && A2Z[i] <= 122){
+                A2Z[i]= A2Z[i] - 32; // making sure all characters are upper case
+           }
+        
+            if(A2Z[i] >= 65 && A2Z[i] <= 90){ // Only change Uppercase letter caracters to keep punctuation  
+                A2Z[i] = A2Z[i] + key; //adds the key to the char in the array at [i]
+    
+                if(A2Z[i] > 'Z'){ // if statement regarding looping (if it goes past z it will go back to a)
+                    A2Z[i]= A2Z[i] - 26;
+                }
+            }
+        }
+    printf("%s\n", A2Z);   
+    
+        break; // Jump out of switch()
+    
+    case 2:{ // Jumped to if x == 2
+        //printf("Enter code: ");
+        //scanf("%s", &A2Z);
+        for (i = 0; A2Z[i] != '\0'; i++) // starting a counter for how ever large A2Z is
+        {
+            //printf("%c, %d, --> %c, %d\n", A2Z[i] , A2Z[i], A2Z[i]+key , A2Z[i]+key); // displays what is happeing in the workings of the code
+            //printf("%s\n", A2Z);
+            if(A2Z[i] >= 97 && A2Z[i] <= 122){
+                A2Z[i]= A2Z[i] - 32; // making sure all characters are upper case
+            }
+            
+            if(A2Z[i] >= 65 && A2Z[i] <= 90){ // Only change Uppercase letter caracters to keep punctuation  
+                A2Z[i] = A2Z[i] + dkey; //adds the key to the char in the array at [i]
+        
+                if(A2Z[i] > 'Z'){ // if statement regarding looping (if it goes past z it will go back to a)
+                    A2Z[i]= A2Z[i] - 26;
+                }
+            }
+        }
+
     printf("%s\n", A2Z);
-    if(A2Z[i] == 32){ // if there is a space it will return to a space
-        A2Z[i]= A2Z[i] - key;
     }
 
-    A2Z[i] = A2Z[i] + key; //adds the key to the char in the array at [i]
-    
-    if(A2Z[i] > 'Z'){ // if statement regarding looping (if it goes past z it will go back to a)
-        A2Z[i]= A2Z[i] - 26;
-    }
-     
-
-printf("%s\n", A2Z);
-
+        break;
+    case 3: // Jumped to if x == 3
+        break;
+    default: return (0); // Jumped to if nothing
 }
 }
-    
+}
+
+
+
