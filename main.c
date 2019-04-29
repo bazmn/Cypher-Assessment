e622835fd46c80715cf9986f9b70567daf795bac
Sebastian Scott <sebastian.r.scott@gmail.com>
2019-04-29 01:26:35 +0000

    master: Working Cesar Cypher decoder for unknown key

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 int i; 
-int key = 15; // how much the array will move
+int crash = 0;
+int key = 2; // how much the array will move
+//char A2Z[400]; //first array
 char A2Z[] = "WX BJB & SPS, NDJ'GT IWT QTHI"; //first array
+int Switch = 0;
 
 
 int main()
 {
-int dkey = 26 - key;    
-switch(2) {
-    case 1:{
+    //printf("Type 1 for Cesar Cyper Incryption\nType 2 for Cesar Cyper Decyption with known key\n"Type 2 for Cesar Cyper Decyption with unknown key\n");
+    //scanf("%d", Switch);
     //printf("Enter code: ");
-    //scanf("%s", &A2Z);
+    //scanf("%c", &A2Z);
+int dkey = 26 - key;    
+
+
+switch(3) {
+    case 1:{
+
         for (i = 0; A2Z[i] != '\0'; i++) // starting a counter for how ever large A2Z is
         {
         //printf("%c, %d, --> %c, %d\n", A2Z[i] , A2Z[i], A2Z[i]+key , A2Z[i]+key); // displays what is happeing in the workings of the code
@@ -29,14 +37,13 @@ switch(2) {
             }
         }
     printf("%s\n", A2Z);   
-    
+}
         break; // Jump out of switch()
     
     case 2:{ // Jumped to if x == 2
         //printf("Enter code: ");
         //scanf("%s", &A2Z);
-        for (i = 0; A2Z[i] != '\0'; i++) // starting a counter for how ever large A2Z is
-        {
+        for (i = 0; A2Z[i] != '\0'; i++){ // starting a counter for how ever large A2Z is       
             //printf("%c, %d, --> %c, %d\n", A2Z[i] , A2Z[i], A2Z[i]+key , A2Z[i]+key); // displays what is happeing in the workings of the code
             //printf("%s\n", A2Z);
             if(A2Z[i] >= 97 && A2Z[i] <= 122){
@@ -53,15 +60,41 @@ switch(2) {
         }
 
     printf("%s\n", A2Z);
+    
     }
 
         break;
-    case 3: // Jumped to if x == 3
+    case 3:{ // Jumped to if x == 2
+        //printf("Enter code: ");
+        //scanf("%s", &A2Z);
+        for (crash = 0; crash <= 26; crash++){  
+            for (i = 0; A2Z[i] != '\0'; i++){ // starting a counter for how ever large A2Z is                   
+                        //printf("%c, %d, -->+%d--> %c, %d\n", A2Z[i] , A2Z[i], key, A2Z[i]+key , A2Z[i]+key); // displays what is happeing in the workings of the code
+                        //printf("%s\n", A2Z);
+                if(A2Z[i] >= 97 && A2Z[i] <= 122){
+                    A2Z[i]= A2Z[i] - 32; // making sure all characters are upper case
+                }
+                
+                if(A2Z[i] >= 65 && A2Z[i] <= 90){ // Only change Uppercase letter caracters to keep punctuation  
+                    A2Z[i] = A2Z[i] + 1; //adds the key to the char in the array at [i]
+                    
+                    if(A2Z[i] > 'Z'){ // if statement regarding looping (if it goes past z it will go back to a)
+                        A2Z[i]= A2Z[i] - 26;
+                    }
+                }
+            }
+            
+        printf("%s\n", A2Z);
+        }
+    }
+
+
         break;
     default: return (0); // Jumped to if nothing
+
 }
 }
-}
+
 
 
 
